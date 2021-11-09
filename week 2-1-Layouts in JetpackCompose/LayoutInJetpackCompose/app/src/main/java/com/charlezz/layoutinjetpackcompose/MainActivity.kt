5425bbe6573e7e40be8c3d48c626c9af6873aab5
Charlezz <soohwan.ok@navercorp.com>
2021-11-10 02:40:08 +0900

    Layouts in Jetpack Compose - Working with lists

--- week 2-1-Layouts in JetpackCompose/LayoutInJetpackCompose/app/src/main/java/com/charlezz/layoutinjetpackcompose/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutInJetpackCompose/app/src/main/java/com/charlezz/layoutinjetpackcompose/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LayoutInJetpackComposeTheme {
-                LayoutsCodelab()
+                ScrollingList()
             }
         }
     }
@@ -69,6 +69,59 @@ fun BodyContent(modifier: Modifier = Modifier) {
 @Composable
 fun LayoutsCodelabPreview() {
     LayoutInJetpackComposeTheme {
-        LayoutsCodelab()
+//        LayoutsCodelab()
+        ScrollingList()
+    }
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(Modifier.width(10.dp))
+        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Composable
+fun ScrollingList() {
+    val listSize = 100
+    // We save the scrolling position with this state
+    val scrollState = rememberLazyListState()
+    // We save the coroutine scope where our animated scroll will be executed
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+        Row {
+            Button(onClick = {
+                coroutineScope.launch {
+                    // 0 is the first item index
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text("Scroll to the top")
+            }
+
+            Button(onClick = {
+                coroutineScope.launch {
+                    // listSize - 1 is the last index of the list
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }) {
+                Text("Scroll to the end")
+            }
+        }
+
+        LazyColumn(state = scrollState) {
+            items(listSize) {
+                ImageListItem(it)
+            }
+        }
     }
 }
